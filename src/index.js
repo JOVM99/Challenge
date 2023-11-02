4bfa9abc2a8032bd9aacf74eb3b873fa53daebfd
JOVM99 <jvieramejia@gmail.com>
2023-11-02 16:02:18 -0600

    :sparkles: List and add tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const connectDB = require("./bd/database.config");
+const Todo = require("./bd/schema");
 const app = express();
 const path = require("path");
 const port = 3000;
 
-app.use(express.json());
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("src/public"));
+
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (error) {
+    return res.status(500).send(err);
+  }
+});
+
+app.post("/api/todos", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  try {
+    const todo = await newTodo.save();
+    res.json(todo)
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
 
 const bootstrap = async () => {
---
+++ src/public/script.js
@@ -0,0 +1,40 @@
+const getTasks = async () => {
+  const res = await fetch("/api/todos");
+  const data = await res.json();
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+  data.forEach((todo) => {
+    const listItem = document.createElement("li");
+    listItem.classList.add('list-group-item');
+    listItem.innerHTML = `
+        <input
+            class="form-check-input mx-2"
+            type="checkbox"
+            ${todo.completed ? "checked" : ""} onchange="updateTask('${
+      todo._id
+    }')"
+          />
+          <label class="form-check-label" for="flexCheckChecked">
+          ${todo.task}
+          </label>
+      `;
+    list.appendChild(listItem);
+  });
+};
+
+const addTask = async () => {
+  const newTask = document.getElementById("task").value;
+  if (newTask.trim() !== "") {
+    const res = await fetch("/api/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ task: newTask, completed: false }),
+    });
+    await res.json();
+    document.getElementById("task").value = "";
+    getTasks();
+  }
+};
+
+getTasks();
+
